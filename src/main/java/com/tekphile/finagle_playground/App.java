017e44b49e3ea19d20c2c31b46190154ad1fbe93
Kevin Stanton <stantonk@gmail.com>
2014-08-11 23:45:48 -0500

    finagle-http java example

--- src/main/java/com/tekphile/finagle_playground/App.java
+++ src/main/java/com/tekphile/finagle_playground/App.java
@@ -1,13 +1,21 @@
 package com.tekphile.finagle_playground;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.twitter.finagle.Service;
+import com.twitter.util.Future;
+import org.jboss.netty.handler.codec.http.DefaultHttpResponse;
+import org.jboss.netty.handler.codec.http.HttpRequest;
+import org.jboss.netty.handler.codec.http.HttpResponse;
+import org.jboss.netty.handler.codec.http.HttpResponseStatus;
+
+public class App {
+
+    public static void main(String[] args) {
+        Service<HttpRequest, HttpResponse> service = new Service<HttpRequest, HttpResponse>() {
+            @Override
+            public Future<HttpResponse> apply(HttpRequest request) {
+                return Future.value(new DefaultHttpResponse(request.getProtocolVersion(), HttpResponseStatus.OK));
+            }
+        };
     }
+
 }
