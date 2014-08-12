c56c6f2f0256f701debff0a2af83fd40eb559995
Kevin Stanton <stantonk@gmail.com>
2014-08-12 00:30:16 -0500

    java and scala examples working

--- src/main/java/com/tekphile/finagle_playground/FinagleHttpServiceJavaExample.java
+++ src/main/java/com/tekphile/finagle_playground/FinagleHttpServiceJavaExample.java
@@ -5,6 +5,7 @@ import com.twitter.finagle.ListeningServer;
 import com.twitter.finagle.Service;
 import com.twitter.util.Await;
 import com.twitter.util.Future;
+import com.twitter.util.TimeoutException;
 import org.jboss.netty.handler.codec.http.DefaultHttpResponse;
 import org.jboss.netty.handler.codec.http.HttpRequest;
 import org.jboss.netty.handler.codec.http.HttpResponse;
@@ -12,16 +13,22 @@ import org.jboss.netty.handler.codec.http.HttpResponseStatus;
 
 public class FinagleHttpServiceJavaExample {
 
-    public static void main(String[] args) { //throws InterruptedException {
-//        Service<HttpRequest, HttpResponse> service = new Service<HttpRequest, HttpResponse>() {
-//            @Override
-//            public Future<HttpResponse> apply(HttpRequest req) {
-//                return Future.value(new DefaultHttpResponse(req.getProtocolVersion(), HttpResponseStatus.OK));
-//            }
-//        };
-//
-//        ListeningServer server = Http.serve(":8080", service);
-//        Await.ready(server);
+    public static void main(String[] args) throws InterruptedException {
+        Service<HttpRequest, HttpResponse> service = new Service<HttpRequest, HttpResponse>() {
+            @Override
+            public Future<HttpResponse> apply(HttpRequest req) {
+                // must parameterize Future to HttpResponse
+                return Future.<HttpResponse>value(new DefaultHttpResponse(req.getProtocolVersion(), HttpResponseStatus.OK));
+            }
+        };
+
+        ListeningServer server = Http.serve(":8080", service);
+        try {
+            Await.ready(server);
+        } catch (TimeoutException e) { //TODO: submit bug ticket for intellij (first try upgrading)
+            e.printStackTrace();
+
+        }
     }
 
 }
