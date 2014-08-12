f0a6bceb89148609b90808bc2a908792c78f61b1
Kevin Stanton <stantonk@gmail.com>
2014-08-11 23:20:29 -0500

    initial commit

---
+++ src/main/java/com/tekphile/finagle_playground/App.java
@@ -0,0 +1,13 @@
+package com.tekphile.finagle_playground;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
